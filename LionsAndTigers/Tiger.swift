687928e16b4d2ea04abbe989707418f3442e6364
gnazarkin <gleb.nazarkin@gmail.com>
2014-12-12 12:35:20 -0500

    Additional features

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,65 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Gleb Nazarkin on 2014-12-11.
+//  Copyright (c) 2014 Addo Labs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+    
+    func chuff() {
+        println("Tiger: Chuff Chuff")
+    }
+    
+    func chuffANumberOfTimes (numberOfTimes: Int) {
+        for var chuff = 0; chuff < numberOfTimes; ++chuff {
+            self.chuff()
+        }
+    }
+    
+    func chuffAtNumberOftimes(numberOfTimes: Int, isLoud: Bool) {
+        for var chuffTimes = 1; chuffTimes <= numberOfTimes; chuffTimes++ {
+            if isLoud {
+                chuff()
+            } else {
+                println("Tiger: purr purr")
+            }
+        }
+    }
+    
+    func ageInTigerYearsFromAge(regularAge: Int) -> Int {
+        
+        let newAge = regularAge * 3
+        return newAge
+    }
+    
+    func randomFact() -> String {
+        let randomNumber = Int(arc4random_uniform(UInt32(3)))
+        
+        var randomFact:String
+        
+        if randomNumber == 0 {
+            randomFact = "The Tiger is the biggest species in the cat family"
+        }
+        else if randomNumber == 1{
+            randomFact = "Tigers can reach a length of 3.3 meters"
+        }
+        else {
+            randomFact = "A group of tigers is known as an 'ambush' or 'streak'"
+        }
+        
+        return randomFact
+    }
+    
+    
+    
+    
+}
